f5b3acd444c0a987e051df194e438d17ebedec90
krishnakanth200 <80898261+krishnakanth200@users.noreply.github.com>
2024-08-14 10:45:30 +0530

    Update userRoutes.js

--- Pizza-Store( Backend)/routes/userRoutes.js
+++ Pizza-Store( Backend)/routes/userRoutes.js
@@ -1,23 +1,13 @@
 const express = require('express');
-const {
-    createOrder,
-    getOrders,
-    getOrderById,
-    updateOrderStatus,
-    deleteOrder
-} = require('../controllers/orderControllers');
+const { registerUser, loginUser, getUserProfile } = require('../controllers/userControllers');
 const { protect, authorize } = require('../middleware/authMiddleware');
-
 const router = express.Router();
 
 // Public routes
-// (none, all order routes are protected)
+router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 // Protected routes
-router.post('/', protect, authorize('admin'), createOrder); // Create an order
-router.get('/', protect, authorize('admin'), getOrders); // Get all orders (Admin only)
-router.get('/:orderId', protect, authorize('admin'), getOrderById); // Get order by ID (Admin only)
-router.put('/:orderId', protect, authorize('admin'), updateOrderStatus); // Update order status (Admin only)
-router.delete('/:orderId', protect, authorize('admin'), deleteOrder); // Delete an order (Admin only)
+router.get('/profile', protect, getUserProfile);
 
 module.exports = router;
