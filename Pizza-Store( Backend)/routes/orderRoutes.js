bf1bd0798bd0c531838001f0afef6a9988fb82b7
krishnakanth200 <80898261+krishnakanth200@users.noreply.github.com>
2024-08-14 10:45:59 +0530

    Update orderRoutes.js

--- Pizza-Store( Backend)/routes/orderRoutes.js
+++ Pizza-Store( Backend)/routes/orderRoutes.js
@@ -1,13 +1,23 @@
 const express = require('express');
-const { registerUser, loginUser, getUserProfile } = require('../controllers/userControllers');
-const { protect } = require('../middleware/authMiddleware');
+const {
+    createOrder,
+    getOrders,
+    getOrderById,
+    updateOrderStatus,
+    deleteOrder
+} = require('../controllers/orderControllers');
+const { protect, authorize } = require('../middleware/authMiddleware');
+
 const router = express.Router();
 
 // Public routes
-router.post('/register', registerUser);
-router.post('/login', loginUser);
+// (none, all order routes are protected)
 
 // Protected routes
-router.get('/profile', protect, getUserProfile);
+router.post('/', protect, authorize('admin'), createOrder); // Create an order
+router.get('/', protect, authorize('admin'), getOrders); // Get all orders (Admin only)
+router.get('/:orderId', protect, authorize('admin'), getOrderById); // Get order by ID (Admin only)
+router.put('/:orderId', protect, authorize('admin'), updateOrderStatus); // Update order status (Admin only)
+router.delete('/:orderId', protect, authorize('admin'), deleteOrder); // Delete an order (Admin only)
 
 module.exports = router;
